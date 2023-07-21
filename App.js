e5570bd5489891db2150477e3880a0190bc89f88
anthony <anthonytguilherme@hotmail.com>
2023-07-21 16:32:00 -0300

    customize the button change camera

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
-
+  const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [cpaturedPhoto, setCapturedPhoto] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync()
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === "granted");
-    })
+    })();
   }, []);
 
   if (hasPermission === null) {
@@ -27,7 +29,20 @@ export default function App() {
         style={styles.camera}
         type={type}
       >
-
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <FontAwesome name="exchange" size={23} color="red"></FontAwesome>
+          </TouchableOpacity>
+        </View>
       </Camera>
     </SafeAreaView>
   );
@@ -41,5 +56,22 @@ const styles = StyleSheet.create({
   camera: {
     width: "100%",
     height: "100%",
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
   }
 });
