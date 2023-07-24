da2610154172a236e6d72f919a036c60035f4608
anthony <anthonytguilherme@hotmail.com>
2023-07-24 14:17:59 -0300

    styling all camera components

--- App.js
+++ App.js
@@ -123,5 +123,21 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 50,
+  },
+  contentModal: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    margin: 20,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    left: 2,
+    margin: 10,
+  },
+  imgPhoto: {
+    width: "100%",
+    height: 400,
   }
 });
